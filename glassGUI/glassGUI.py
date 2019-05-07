f93cc2edbeae676d24b66642ee06b82d3ae96b16
Jee Wen Jie <40534826+jeewenjie@users.noreply.github.com>
2019-05-07 11:42:00 +0800

    first commit
    Requires a trained keras model to run

---
+++ glassGUI/glassGUI.py
@@ -0,0 +1,189 @@
+from kivy.lang import Builder
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.garden.graph import MeshLinePlot
+from kivy.clock import Clock
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
+from kivy.properties import ListProperty
+
+from threading import Thread
+import audioop
+import pyaudio
+import librosa
+
+from keras.models import load_model
+import tensorflow as tf
+
+import numpy as np
+import wave
+
+LABELS = ['Glass',' '] # Others, Siren, Honks
+
+def get_microphone_level():
+    """
+    source: http://stackoverflow.com/questions/26478315/getting-volume-levels-from-pyaudio-for-use-in-arduino
+    audioop.max alternative to audioop.rms
+    """
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    DEVICE_IDX = 0
+    p = pyaudio.PyAudio()
+
+    s = p.open(format=FORMAT,
+               channels=CHANNELS,
+               rate=RATE,
+               input=True,
+               input_device_index = DEVICE_IDX,
+               frames_per_buffer=CHUNK)
+
+    global levels
+    global levels_2
+    global four_sec
+    global temp_levels
+
+    while True:
+        data = s.read(CHUNK,exception_on_overflow=False)
+
+        mx = audioop.rms(data, 2)/255
+
+        if len(levels_2) >= int(RATE*2/CHUNK):
+
+            temp_levels.extend(levels_2)
+ 
+            four_sec = temp_levels
+
+            temp_levels = levels_2
+          
+            levels_2 = []
+
+        if len(levels) >= 100:
+            levels = []
+
+        levels.append(mx)
+        levels_2.append(data)
+
+	# pass to model
+	# get results
+	# if result: change label
+	# else: continue
+
+class Logic(BoxLayout):
+    pred_label = StringProperty()
+    bg_color = ListProperty()
+    font_color = ListProperty()
+
+    def __init__(self,):
+        super(Logic, self).__init__()
+        self.plot = MeshLinePlot(color=[1, 1, 1, 1])
+        self.pred_label = ' '
+        self.bg_color = [0,0,0,1]
+        self.font_color = [1,1,1,1]
+
+    def start(self):
+        self.ids.graph.add_plot(self.plot)
+        Clock.schedule_interval(self.get_value, 0.1)
+
+        Clock.schedule_interval(self._listen, 2)
+
+    def stop(self):
+        Clock.unschedule(self.get_value)
+        Clock.unschedule(self._listen)
+
+    def get_value(self, dt):
+        self.plot.points = [(i, j) for i, j in enumerate(levels)]
+	
+    def _listen(self,dt):
+
+        X = np.empty(shape=(1,128,63,1,1))
+	    
+        input_length = 176400 # Sampling rate * Audio duration = 44100 * 4 = 176400 
+		
+        global four_sec
+
+        wf = wave.open('output.wav', 'wb')
+        wf.setnchannels(7)
+        wf.setsampwidth(pyaudio.PyAudio().get_sample_size(pyaudio.paInt16))
+        wf.setframerate(44100)
+        wf.writeframes(b''.join(four_sec))
+        wf.close()
+        #fname = 'models/clips/7389-1-0-2.wav'
+
+        #four_sec, _ = librosa.core.load(fname, sr=44100, res_type="kaiser_fast")
+        #data = np.transpose(four_sec)		
+
+        data, _ = librosa.core.load('output.wav', sr=44100, res_type="kaiser_fast")
+		# preprocessing
+        if len(data) > input_length:
+            max_offset = len(data) - input_length
+            offset = np.random.randint(max_offset)
+            data = data[offset:(input_length+offset)]
+        else:
+            if input_length > len(data):
+               max_offset = input_length - len(data)
+               offset = np.random.randint(max_offset)
+            else:
+               offset = 0
+            data = np.pad(data, (offset, input_length - len(data) - offset), "constant")
+        #y = librosa.util.normalize(data)
+        
+        #y = np.array(data)
+        #y = y/255
+        y = librosa.util.normalize(data)        
+        #y = librosa.util.buf_to_float(y,n_bytes=8)
+        print(y)
+		# convert to melspec
+        melspec = librosa.feature.melspectrogram(y = y, sr = 44100, n_fft=2048, hop_length=1024, n_mels=120)
+	   
+        log_melspec = np.log(melspec.T + 1e-10)
+	    
+        log_melspec = np.array([log_melspec])
+        log_melspec = np.transpose(log_melspec)
+        y = log_melspec
+	    
+        y = np.expand_dims(y, axis=-1)
+
+        X[0,] = y
+        
+        X = np.squeeze(X, axis =-1)
+
+        with graph.as_default(): # For circumventing keras's problem
+             predictions = model.predict(X,batch_size=1,verbose=1)
+
+        top_1 = np.array(LABELS)[np.argsort(-predictions, axis=1)[:, :1]]
+        the_prediction = ([' '.join(list(x)) for x in top_1])[0]
+        if the_prediction == 'Siren':
+           self.pred_label = the_prediction
+           self.bg_color = [1,0,0,1]
+           self.font_color = [0,0,0,1]
+        else:
+        	self.pred_label = the_prediction
+        	self.bg_color = [0,0,0,1]
+        	self.font_color = [1,1,1,1]
+
+        print(self.pred_label)
+
+class RealTimeMicrophone(App):
+    def build(self):
+        return Builder.load_file("MyWidget.kv")
+
+
+if __name__ == "__main__":
+    
+    # Loading stuff
+    model = load_model('models/best_ais_8k_new.h5')
+    model.load_weights('models/best_ais_8k_new.h5')
+    graph = tf.get_default_graph()
+
+    temp_levels = []
+    four_sec = []
+    levels = []  # store levels of microphone
+    levels_2 = []
+
+    get_level_thread = Thread(target = get_microphone_level)
+    get_level_thread.daemon = True
+    get_level_thread.start()
+
+    RealTimeMicrophone().run()
